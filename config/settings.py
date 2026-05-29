fd9d270f9160850d23d98b8a0ae73cb4efe66a16
Вова Тупісь <vovatupis@MacBook-Pro-Vova.local>
2026-05-29 14:26:00 +0300

    feat: install drf-spectacular and configure swagger api documentation

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
 
     'rest_framework',
 
@@ -137,3 +138,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Airport API Project',
+    'DESCRIPTION': 'Документація для нашого авіаційного API (Locations & Fleet)',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
