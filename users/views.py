c32459dbb1fd21b3b59a1ecc63b44d36bbcd6994
Вова Тупісь <vovatupis@MacBook-Pro-Vova.local>
2026-06-23 13:24:26 +0300

    feat: add register action to UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-from drf_spectacular.utils import extend_schema_view, extend_schema
 from users.serializers import UserSerializer, UserRegisterSerializer
 
 User = get_user_model()
@@ -18,3 +19,10 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action == 'register':
             return [AllowAny()]
         return [IsAdminUser()]
+
+    @action(detail=False, methods=['post'], url_path='register')
+    def register(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
